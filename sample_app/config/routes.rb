ac9794ec3a91b5f59766fc9e13ec66a0b32b2843
joaoBordalo <bordalo.joao94@gmail.com>
2017-01-06 15:59:13 +0000

    star over

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,17 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get '/contact', to: 'static_pages#contact'
-
-  root 'static_pages#home'
-
-  get '/about', to: 'static_pages#about'
-
-  get '/help', to: 'static_pages#help'
-
-  get '/signup', to: 'users#new'
-
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- # root 'application#hello'
 end
---
+++ sample_app/mountable.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sshfs clouduser@i-664.cloud.fraunhofer.pt:/home/clouduser/workspace/github/starting_with_rails/sample_app/ .
+echo "shared sample_app folder from clouduser@i-664.cloud.fraunhofer.pt with success"
